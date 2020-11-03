6aad1996684d48c67dd887d1a65979827ea5b07d
Comforter11 <71063972+Comforter11@users.noreply.github.com>
2020-11-03 00:07:26 -0800

    Add files via upload

---
+++ cli.py
@@ -0,0 +1,11 @@
+reader = open('data/zen-python.txt')
+
+reader.write
+for line in reader:
+    print(line, end='')
+
+
+# data/zen-python.txt longest_word
+def longest_word(string):
+    print(longest_word, string='')
+    print(string)
